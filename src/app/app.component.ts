b63ea3aaf48761c926004954a34bc1e6d25dba72
zebn <zebn@bk.ru>
2022-11-24 19:26:40 +0100

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tarea2';
+  serverId: number = 10;
+  serverName='';
+  allowNewServer=false;
+  serverStatus: string = 'offLine';
+  onUpdateServerName(event:Event)
+{
+this.serverName = (<HTMLInputElement>event.target).value;
+if (this.serverName) this.allowNewServer = true; else this.allowNewServer = false;
+
 }
+  }
+
