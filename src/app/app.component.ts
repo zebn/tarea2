6152891cfdbea381b57c191035f17ed1a119e874
zebn <zebn@bk.ru>
2022-11-24 19:27:03 +0100

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,15 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('userNameInput') inputName;
   serverId: number = 10;
   serverName='';
   allowNewServer=false;
   serverStatus: string = 'offLine';
   onUpdateServerName(event:Event)
-{
-this.serverName = (<HTMLInputElement>event.target).value;
-if (this.serverName) this.allowNewServer = true; else this.allowNewServer = false;
+    {
+    this.serverName = (<HTMLInputElement>event.target).value;
+    if (this.serverName) this.allowNewServer = true; else this.allowNewServer = false;
 
-}
+    }
+
+   resetUser()
+    {
+      this.inputName.nativeElement.value = '';
+      this.allowNewServer = false;
+      this.serverName = '';
+    }
+
   }
 
